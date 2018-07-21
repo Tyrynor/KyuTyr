e07e417678b7f3a3e28f817f1e76bc1de269cf46
quent <quent@DESKTOP-FHUJTO5>
2018-07-22 00:06:38 +0200

    launcher : Ajout du main

---
+++ src/launcher/Launcher.java
@@ -0,0 +1,20 @@
+package launcher;
+
+/**
+ * 
+ * @author Tyrynor, 21/07/2018
+ *
+ */
+public class Launcher {
+
+	public static String MY_STRING = "Bite";
+	
+	/**
+	 * Lanceur de l'application
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(Launcher.MY_STRING);
+	}
+
+}
