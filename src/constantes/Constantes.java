19aabab803465d6dd44caa78d1280194fc3bdf69
Tyrynor <quentinferry@hotmail.fr>
2018-08-07 15:07:43 +0200

    Ajout de commentaires Constantes.java

--- src/constantes/Constantes.java
+++ src/constantes/Constantes.java
@@ -3,7 +3,7 @@ package constantes;
  * 
  * @author Tyrynor, 22/07/2018
  * History:
- *  - Tyrynor, 22/07/2018 Cr�ation
+ *  - Tyrynor, 22/07/2018 Création
  *
  */
 
@@ -11,6 +11,7 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 
 public final class Constantes {
+	// Constantes utiles à l'application
 	public static final String IHM_NOM_FENTRE = "Nom de fenetre";
 	public static final Dimension IHM_TAILLE_FENETRE = Toolkit.getDefaultToolkit().getScreenSize();
 	public static final Dimension TAILLE_BASE_ENTITY = new Dimension(50,50);
